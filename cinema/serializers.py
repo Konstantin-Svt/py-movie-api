7d93950f85c97831f0bbf91773df12f20a2eb485
Konstantin S. <kostaho16@gmail.com>
2025-12-15 13:24:51 +0200

    Solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -7,7 +7,7 @@ class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(max_length=100)
     description = serializers.CharField(max_length=255)
-    duration = serializers.IntegerField()
+    duration = serializers.IntegerField(min_value=0)
 
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
